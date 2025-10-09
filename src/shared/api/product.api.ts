e3cd97ef227c4c6a7b7d1f226194f9ca99fcb0d9
abdulazizkhatamov <abdulazizkhatamov@hotmail.com>
2025-10-09 20:28:21 +0500

    ....

--- src/shared/api/product.api.ts
+++ src/shared/api/product.api.ts
@@ -3,7 +3,6 @@ import type { Product } from '../schema/product.schema'
 import type { PaginatedProducts } from '../schema/pagination.schema'
 import axiosInstance from '@/config/axios.config'
 
-
 const fetchProducts = async ({
   pageParam = null,
   categoryId,
